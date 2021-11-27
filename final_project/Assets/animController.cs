e6878c8947303e135aed3bb9c2b7ced49da94a6e
guillermogut <guillermogut@umass.edu>
2021-11-27 12:21:00 -0500

    added game character
    Meet our waifu ( yes, you heard right ). No, but seriously this is just a test of the character controller and animations. No real rpg elements yet

---
+++ final_project/Assets/animController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour
+{
+
+    public Animator anim;
+    public bool isWalking;
+    public bool isRunning;
+    public bool isIdle;
+    public bool isAiming;
+
+    public GameObject weapon;
+    /// <weapon ready/ not ready>
+    public Transform WReady;
+    public Transform WNotReady;
+    /// </summary>
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        WNotReady.transform.position = weapon.transform.position;
+        WNotReady.transform.rotation = weapon.transform.rotation;
+        //Vector3(-0.00185999996, 0.00307999994, 0.00307000009);//pos  Vector3(-0.00243000011,0.00319999992,0.00255999994)
+        //Quaternion(0, -0.452434748, 0, 0.891797543);//rotation   Quaternion(-0.218622565,-0.426985323,0.193708897,0.855783105)
+        isWalking = false;
+        isRunning = false;
+        isIdle = true;
+        isAiming = false;
+
+        anim = GetComponent<Animator>();
+
+        anim.Play("idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (isWalking)
+        {
+            anim.Play("walking");
+            isRunning = false;
+            isIdle = false;
+        }
+        
+        if(isRunning)
+        {
+            anim.Play("Rifle Run");
+            isWalking = false;
+            isIdle = false;
+        }
+        if(isIdle)
+        {
+            anim.Play("idle");
+            isWalking = false;
+            isRunning = false;
+            
+        }
+        if(isAiming)
+        {
+            weapon.transform.localPosition = new Vector3(-0.00243000011f,0.00319999992f,0.00255999994f);//pos
+            weapon.transform.localRotation = new Quaternion(-0.218622565f, -0.426985323f, 0.193708897f, 0.855783105f);//rotation
+            anim.Play("Firing Rifle");
+            isWalking = false;
+            isRunning = false;
+            isIdle = false;
+        }
+        else
+        {   anim.StopPlayback();
+            weapon.transform.localPosition = new Vector3(0.000230000005f, -0.00013f, 0.00179000001f);
+            weapon.transform.localRotation = new Quaternion(0.372742593f, -0.481262475f, 0.236914799f, 0.757179439f);
+            
+            //return;
+        }
+    }
+}
---
+++ final_project/Assets/player.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Animations.Rigging;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject playerObj;
+    public GameObject currentMap;
+    private Animator animator;
+    
+    public Vector3 pos;
+    private Vector3 destination;
+    private float speed;//speed
+    private Vector3 lookVector;//WHERE SHE LOOKIN AT
+    private GameObject lookArrow;
+    private bool isMoving;
+
+    
+
+    void Start()
+    {
+
+        Time.timeScale = 1f;
+        //animator = GetComponent<Animator>();
+        playerObj.transform.position = new Vector3(0f,0f,0f);
+        pos = playerObj.transform.position;
+        //lookArrow = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        //lookArrow.transform.localScale = new Vector3(.2f, .2f, 1f);
+        //lookArrow.transform.position = pos;
+
+        isMoving = false;
+        lookVector = new Vector3(0f,0f,1f);
+        speed = 1f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float x = gameObject.transform.position.x;
+        float z = gameObject.transform.position.z;
+        pos = new Vector3(x, 0f, z);
+
+        if (isMoving)
+        {
+            //animator.SetBool("idle", false);
+            //animator.SetBool("isWalking", true);
+            this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            Debug.Log("click");
+            if(Vector3.Distance(pos, destination) < .05f)
+            {
+                //animator.SetBool("isWalking", false);
+                //animator.SetBool("idle", true);
+                GetComponent<animController>().isIdle = true;
+                GetComponent<animController>().isRunning = false;
+                GetComponent<animController>().isWalking = false;
+                isMoving = false;
+            }
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            GetComponent<animController>().isAiming = false;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hit))
+            {
+                if(hit.collider.name == currentMap.name)
+                {
+                    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    sphere.transform.localScale = new Vector3(.2f, .2f, .2f);
+                    sphere.transform.position = hit.point;
+
+
+
+                    Vector3 dest = hit.point;
+                    dest.y = 0f;
+
+
+                    Destroy(sphere, 10f);
+                    turnPlayer(hit.point);
+                    GetComponent<animController>().isRunning = true;
+                    isMoving = true;
+
+
+                }
+
+
+            }
+
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<RigBuilder>().enabled = false;
+            isMoving = false;
+            GetComponent<animController>().isWalking = false;
+            GetComponent<animController>().isRunning = false;
+            GetComponent<animController>().isIdle = false;
+            GetComponent<animController>().isAiming = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<RigBuilder>().enabled = true;
+        }
+    }
+
+    void turnPlayer(Vector3 dest)
+    {
+
+        destination = dest;
+        transform.LookAt(dest);
+
+
+    }
+
+    void renderVect(Vector3 vToRend)
+    {
+        GameObject cyl = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        cyl.transform.localScale = new Vector3(.2f, .2f, 1f);
+        cyl.transform.position = pos;
+    }
+}
